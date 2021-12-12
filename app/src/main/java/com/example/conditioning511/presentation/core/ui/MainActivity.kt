e017f669b2ec30a433deeabf75c2af4136fd2daa
Ivan <iaabramov_2@edu.hse.ru>
2021-12-12 12:42:14 +0300

    Поправлены небольшие ошибки

--- app/src/main/java/com/example/conditioning511/presentation/core/ui/MainActivity.kt
+++ app/src/main/java/com/example/conditioning511/presentation/core/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.conditioning511.presentation.core.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.conditioning511.presentation.core.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
